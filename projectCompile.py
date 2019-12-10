eeddfa328922fd9aa0e8855fa3a1927a02d3e30a
scirocc <m19802253786@163.com>
2019-12-10 17:41:37 +0800

    a

--- projectCompile.py
+++ projectCompile.py
@@ -66,53 +66,55 @@ def findALLIncludeFile(ab_dir):
             findALLIncludeFile(path)
 
 
-def WriteMake():
-    pass
-    # str_ = 'add_executable({}\n'.format(projectName)
-    # for srcFile in set(sSrcFile):
-    #     srcFile = srcFile.replace('\\', '/')
-    #     str_ += srcFile + '\n'
-    # for header in set(sInclude):
-    #     header = header.replace('\\', '/')
-    #     str_ += header + '\n'
-    # str_ += 'D:/ProgramData/Anaconda3/include/Python.h' + '\n'
-    # str_ += ')\n'
-    # str_.replace('i', 'iiiiiiiiiiii')
-    # with open(ab_dir + '/CMakeLists.txt', 'w')as f:
-    #     f.write('cmake_minimum_required(VERSION 3.14)\n')
-    #     f.write('project({})\n'.format(projectName))
-    #     f.write('set(CMAKE_CXX_STANDARD 17)\n')
-    #     f.write('set(CMAKE_EXE_LINKER_FLAGS "-static-libgcc -static-libstdc++")\n')
-    #     f.write('include_directories(include)\n')
-    #     f.write(
-    #         'include_directories(D:/ProgramData/Anaconda3/include)\n')  # 还要把python的inlcude文件夹添加进来，因为有可能和python交互，用到python.h
-    #     for dir in set(sIncludefolder):
-    #         dir = dir.replace('\\', '/')
-    #         f.write('include_directories({})\n'.format(dir))
-    #     f.write('link_directories(bin)\n')
-    #     f.write(
-    #         'link_directories(D:/ProgramData/Anaconda3/libs)\n')  # 还要把python的inlcude文件夹添加进来，因为有可能和python交互，用到python36.lib
-    #     for dir in set(slibFolder):
-    #         dir = dir.replace('\\', '/')
-    #         f.write('include_directories({})\n'.format(dir))
-    #     f.write(str_)  # add_executable信息
-    #     str1_ = 'TARGET_LINK_LIBRARIES({}'.format(projectName) + ' \n'
-    #     for lib in set(sLIB):
-    #         lib = lib.replace('\\', '/')
-    #         str1_ += lib + '\n'
-    #     str1_ += 'python36.lib' + '\n'  # 添加这个东西
-    #     str1_ += 'imagehlp.lib' + '\n'  # 添加这个东西
-    #     str1_ += ')\n'
-    #     f.write('{}'.format(str1_))
-    #     str1_ = 'TARGET_LINK_LIBRARIES({}'.format(projectName) + ' \n'
-    #     for dll in set(sDLL):
-    #         dll = dll.replace('\\', '/')
-    #         str1_ += dll + '\n'
-    #     str1_ += ')\n'
-    #     f.write('{}'.format(str1_))
+def WriteMake(ab_dir,projectName,sSrcFile,sDLL,sLIB,sInclude,slibFolder,sIncludefolder):
+    str_ = 'add_executable({}\n'.format(projectName)
+    for srcFile in set(sSrcFile):
+        srcFile = srcFile.replace('\\', '/')
+        str_ += srcFile + '\n'
+    for header in set(sInclude):
+        header = header.replace('\\', '/')
+        str_ += header + '\n'
+    str_ += 'D:/ProgramData/Anaconda3/include/Python.h' + '\n'
+    str_ += ')\n'
+    str_.replace('i', 'iiiiiiiiiiii')
+    with open(ab_dir + '/CMakeLists.txt', 'w')as f:
+        f.write('cmake_minimum_required(VERSION 3.14)\n')
+        f.write('project({})\n'.format(projectName))
+        f.write('set(CMAKE_CXX_STANDARD 17)\n')
+        f.write('set(CMAKE_EXE_LINKER_FLAGS "-static-libgcc -static-libstdc++")\n')
+        f.write('include_directories(include)\n')
+        f.write(
+            'include_directories(D:/ProgramData/Anaconda3/include)\n')  # 还要把python的inlcude文件夹添加进来，因为有可能和python交互，用到python.h
+        for dir in set(sIncludefolder):
+            dir = dir.replace('\\', '/')
+            f.write('include_directories({})\n'.format(dir))
+        f.write('link_directories(bin)\n')
+        f.write(
+            'link_directories(D:/ProgramData/Anaconda3/libs)\n')  # 还要把python的inlcude文件夹添加进来，因为有可能和python交互，用到python36.lib
+        for dir in set(slibFolder):
+            dir = dir.replace('\\', '/')
+            f.write('include_directories({})\n'.format(dir))
+        f.write(str_)  # add_executable信息
+        str1_ = 'TARGET_LINK_LIBRARIES({}'.format(projectName) + ' \n'
+        for lib in set(sLIB):
+            lib = lib.replace('\\', '/')
+            str1_ += lib + '\n'
+        str1_ += 'python36.lib' + '\n'  # 添加这个东西
+        str1_ += 'imagehlp.lib' + '\n'  # 添加这个东西
+        str1_ += ')\n'
+        f.write('{}'.format(str1_))
+        str1_ = 'TARGET_LINK_LIBRARIES({}'.format(projectName) + ' \n'
+        for dll in set(sDLL):
+            dll = dll.replace('\\', '/')
+            str1_ += dll + '\n'
+        str1_ += ')\n'
+        f.write('{}'.format(str1_))
+    return 0
 
 
 def findEffectiveLocOfSign(sign, str_, startLoc):
+    # print(str_)
+    # print('------------------------')
     loc = str_.find(sign, startLoc)
     if loc == -1:
         return -1
@@ -224,101 +226,81 @@ def findEffectiveLocOfRightBrace(str_, startLoc):#查找有效左大括号位置
     return (loc)
 
 
-def getIndexOfCorreBrace(str_,startloc):#查找对应右括号的位置
-    left_sign_counter = 0
-    right_sign_counter = 0
-    endloc = 0
-    left_sign = '{'
-    right_sign = '}'
-    for index, i in enumerate(str_):
-        if index >=startloc:
-            if i == left_sign :
-                left_sign_counter += 1
-                if left_sign_counter == 1:
-                    startloc = index
-            elif i == right_sign:
-                right_sign_counter += 1
-                if left_sign_counter == right_sign_counter:
-                    endloc = index
-                    break
-    return(endloc)
+def getIndexOfCorreBrace(str_,locOfFirstLeftBrace,left_sign_counter,right_sign_counter):#查找对应右括号的位置
+    #left_sign_counter = 1
+    #right_sign_counter = 0
+    locLeft = findEffectiveLocOfLeftBrace(str_, locOfFirstLeftBrace + 1)
+    locRight = findEffectiveLocOfRightBrace(str_, locOfFirstLeftBrace + 1)
+    if locRight==-1 and locLeft==-1:#全找不到
+        raise ('原cpp文件有错，括号不成对儿')
+    elif locRight==-1 and locLeft!=-1:#
+        raise ('原cpp文件有错，括号不成对儿')
+    elif locRight!=-1 and locLeft==-1:#找得到右，找不到左
+        right_sign_counter += 1
+        if left_sign_counter == right_sign_counter:
+            return (locRight)
+        else:
+            result = getIndexOfCorreBrace(str_,locRight, left_sign_counter, right_sign_counter)
+            return (result)
+    else:#左右全找的到
+        # right_sign_counter+=1
+        # left_sign_counter+=1
+        if locRight<locLeft:
+            right_sign_counter += 1
+            if left_sign_counter == right_sign_counter:
+                return (locRight)
+            else:
+                result = getIndexOfCorreBrace(str_, locRight, left_sign_counter, right_sign_counter)
+                return (result)
+        else:
+            left_sign_counter+=1
+            if left_sign_counter == right_sign_counter:#应该是不可能
+                return (locRight)
+            else:
+                result = getIndexOfCorreBrace(str_, locLeft, left_sign_counter, right_sign_counter)
+                return (result)
 
-def getContentWithOutBrace(str_,startLoc):
-    if findEffectiveLocOfLeftBrace(str_,startLoc)==-1:
-        return str_
-    else:
 
 
 
 
 
-
-
-
-
-
-def dealData(str_):#去掉{}当中的内容
+def getContentWithoutBrace(str_):#去掉{}当中的内容
     sData=str_.split('\n')
-    indexInStr_ = -2
     recursiveMark=False
     lastline=''
+    lineLen=0
+    lineCounter = 0
     for line in sData:
-        indexInStr_ += len(line) + 1
-        #1 同一行包括'namespace'或'class'或‘template’且包括'{' 这样括号间的内容不可删除
-        #2 本行虽然没有'namespace'或'class'，且本行包括'{'，但上一行包括'namespace'或'class'或‘template’,且上一行不含'{' 这样括号间的内容不可删除
-        condition1=(('namespace' in line) or('class' in line)or('template' in line))and ('{' in line)
+        #1 同一行包括'namespace'或'class'且包括'{' 这样括号间的内容不可删除
+        #2 本行虽然没有'namespace'或'class'，且本行包括'{'，但上一行包括'namespace'或'class',且上一行不含'{' 这样括号间的内容不可删除
+        condition1=(('namespace' in line) or('class' in line))and ('{' in line)
         condition2=((('namespace' not in line) and('class' not in line))and('{' in line)and
-                    (('namespace' in lastline)or('class' in lastline)or('template' in lastline))and('{' not in lastline))
+                    (('namespace' in lastline)or('class' in lastline))and('{' not in lastline))
         if condition1 or condition2:#不可删除，看下一行
             pass
         else:
-            # print(line)
-            # print('================================')
-            recursiveMark=True
             indexOfleftBrace = line.find('{')
             if indexOfleftBrace!=-1:#找到了，那么就可以删除
-                index_=indexInStr_-(len(line)-indexOfleftBrace)+1
-                endloc=getIndexOfCorreBrace(str_,index_)
-                str_=str_.replace(str_[index_:endloc+1],'')
-                if recursiveMark == True: str_=dealData(str_)
+                recursiveMark = True
+                indexOfStr=indexOfleftBrace+lineLen+lineCounter
+                endloc=getIndexOfCorreBrace(str_,indexOfStr,1,0)
+                str_=str_.replace(str_[indexOfStr:endloc+1],';')
                 break
+        lineCounter += 1
+        lineLen+=len(line)
         lastline=line
-    return(str_)
+    if not recursiveMark:
+        return(str_)
+    else:
+        return(getContentWithoutBrace(str_))
 
-def gen_str_to_write_in_headerfile(sourceFilePath):
-    with open(sourceFilePath, 'r', encoding='utf-8')as f:
-        datas = f.readlines()
-    sData = [line for line in datas if '#include' not in line]
-    sInclude_str=[line for line in datas if '#include' in line]
-    str_ = "".join(sData)
-    print('============')
-    print(str_)
-    str_=getRidOfTextBetweenDoubleQuote(str_)
-    str_=getRidOfTextBetweenSingleQuote(str_).strip()
 
-    str_=dealData(str_)
-    str_include="".join(sInclude_str)+'\n'
-    str_=str_include+str_
-    return (str_)
-
-def finalAdjustContent(content):
-    sline=content.split('\n')
-    sTemp=[]
-    templateMark=False
-    for line in sline:
-        if line.strip():
-            if (line[0]!='#')and('class' not in line)and('template' not in line)and('namespace' not in line)and(line.strip()[-1]==')'):
-                line=line+';'
-            if('template' in line)and(line.index('template')<line.index('//')):#模板关键字并不在注释中
-                templateMark=True
-        sTemp.append(line)
-    return('\n'.join(sTemp),templateMark)
 
 def reWriteThisCPPFile(sourceFilePath):
     with open(sourceFilePath, 'r', encoding='utf-8')as f:
         datas = f.readlines()
     sData = [line for line in datas if '#include' not in line]
-    sInclude_str = [line for line in datas if '#include' in line]
     print(sourceFilePath.split('\\'))
     headerName=sourceFilePath.split('\\')[-1].replace('.cpp','.h')
     str_='#include"{}"\n'.format(headerName)
@@ -328,34 +310,36 @@ def reWriteThisCPPFile(sourceFilePath):
 
 
 def tell_if_is_templateFile(filepath):
-    with open(filepath,'w',encoding='utf-8')as f:sData=f.readlines()
+    with open(filepath,'r',encoding='utf-8')as f:sData=f.readlines()
     templateMark=False
     for line in sData:
         if line.strip():
-            if ('template' in line) and (line.index('template') < line.index('//')):  # 模板关键字并不在注释中
-                templateMark = True
-                break
+            if ('template' in line):
+                if line.find('//')==-1:# 模板关键字并不在注释中
+                    templateMark = True
+                    break
+                elif line.find('template')<line.find('//'):# 模板关键字并不在注释中
+                    templateMark = True
+                    break
     return (templateMark)
 
 def getHPPFileContent(filepath):
-    with open(filepath,'w',encoding='utf-8')as f:sData=f.readlines()
+    with open(filepath,'r',encoding='utf-8')as f:sData=f.readlines()
     content=''.join(sData)
     return (content)
 
 
 def getHFileContent(filepath):
-    with open(filepath,'w',encoding='utf-8')as f:datas=f.readlines()
+    with open(filepath,'r',encoding='utf-8')as f:datas=f.readlines()
     sData = [line for line in datas if '#include' not in line]
     sInclude_str = [line for line in datas if '#include' in line]
     str_ = "".join(sData)
     str_ = getRidOfTextBetweenDoubleQuote(str_)#去掉正文中的小括号
     str_ = getRidOfTextBetweenSingleQuote(str_).strip()
-
-
-
-
-
-
+    str_=getContentWithoutBrace(str_)
+    str_include="".join(sInclude_str)+'\n'
+    str_=str_include+str_
+    return (str_)
 
 def autoReplenishFile():
     s = sys.argv
@@ -372,6 +356,7 @@ def autoReplenishFile():
                 #2判断源文件中，是否包含template字段，且template字段不在注释中，
                 #如果包含那么就全部复制到HPP文件
                 if tell_if_is_templateFile(file):
+                    print(file,'template')
                     #若为真，那么全部复制进新的hpp文件
                     fileName = corresponding_header2.split('\\')[-1].replace('.hpp', '')
                     sfolder = corresponding_header2.split('\\')
@@ -404,6 +389,8 @@ def autoReplenishFile():
                         f.write('//OriginSrcFile contain template,deprecated')
                     sSrcFile.remove(file)
                 else:#这个时候不含tempalte，采用正常的分离模式
+                    print(file,'withouttemplate')
+
                     fileName = corresponding_header1.split('\\')[-1].replace('.h', '')
                     sfolder = corresponding_header1.split('\\')
                     s = []
@@ -430,61 +417,8 @@ def autoReplenishFile():
 
                         str_ = '\n#endif'
                         f.write(str_)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-            corresponding_header = file.replace('src', 'include').replace('.cpp', '.h')
-            sfolder = corresponding_header.split('\\')
-            s = []
-            for folder in sfolder:
-                s.extend(folder.split('/'))
-            if not os.path.isfile(corresponding_header):  # 没有这些文件，应该新建
-                try:os.makedirs(corresponding_header[:corresponding_header.rfind('\\')])
-                except:pass
-                fileName = corresponding_header.split('\\')[-1].replace('.h', '')
-                sFolder = s
-                sFolder = sFolder[sFolder.index(projectName):-1]
-                print('sFolder:', sFolder)
-                content=gen_str_to_write_in_headerfile(file)
-                content,templateMark=finalAdjustContent(content)
-                if templateMark:
-                    corresponding_header=corresponding_header.replace('.h','.hpp')
-                with open(corresponding_header, 'w')as f:
-                    str_ = '#ifndef '
-                    for folder in sFolder:
-                        str_ += folder.upper() + '_'
-                    str_ += fileName.upper()
-                    str_ += '_H\n'
-                    f.write(str_)
-
-                    str_ = '#define '
-                    for folder in sFolder:
-                        str_ += folder.upper() + '_'
-                    str_ += fileName.upper()
-                    str_ += '_H\n'
-                    f.write(str_)
-
-                    f.write(content)
-
-                    str_ = '\n#endif'
-                    f.write(str_)
-
-                reWriteThisCPPFile(file)#改头
-                if templateMark:#把原来的cpp文件弃用
-                    with open(file,'w')as f:
-                        f.write('//OriginSrcFile contain template,deprecated')
-
+                    reWriteThisCPPFile(file)  # 改头（把原来的include，变为include.h）
+    return 0
 
 def pushShutcut():
     time.sleep(1)
@@ -516,7 +450,7 @@ def examinFolder():
     print('项目头文件夹集合:', sInclude)
     print('项目库文件夹集合:', slibFolder)
     print('项目头文件夹集合:', sIncludefolder)
-    return(sSrcFile,sDLL,sLIB,sInclude,slibFolder,sIncludefolder)
+    return(ab_dir,projectName,sSrcFile,sDLL,sLIB,sInclude,slibFolder,sIncludefolder)
 
 
 
@@ -533,10 +467,10 @@ def main():
     except:pass
     try:os.makedirs(projectDir+'/MyTool/src/')
     except:pass
-    sSrcFile, sDLL, sLIB, sInclude, slibFolder, sIncludefolder=examinFolder()
+    examinFolder()
     autoReplenishFile()
-    WriteMake()
+    ab_dir,projectName,sSrcFile,sDLL,sLIB,sInclude,slibFolder,sIncludefolder=examinFolder()
+    WriteMake(ab_dir,projectName,sSrcFile,sDLL,sLIB,sInclude,slibFolder,sIncludefolder)
     return 0
 
 main()
-
