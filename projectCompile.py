44911bd94184ea1477f967bc24679a8643632d59
scirocc <m19802253786@163.com>
2019-12-13 09:29:34 +0800

    a

--- projectCompile.py
+++ projectCompile.py
@@ -4,6 +4,7 @@ import win32api
 import win32con
 import glob
 import time
+
 # print(123)
 #
 sSrcFile = []
@@ -66,7 +67,7 @@ def findALLIncludeFile(ab_dir):
             findALLIncludeFile(path)
 
 
-def WriteMake(ab_dir,projectName,sSrcFile,sDLL,sLIB,sInclude,slibFolder,sIncludefolder):
+def WriteMake(ab_dir, projectName, sSrcFile, sDLL, sLIB, sInclude, slibFolder, sIncludefolder):
     str_ = 'add_executable({}\n'.format(projectName)
     for srcFile in set(sSrcFile):
         srcFile = srcFile.replace('\\', '/')
@@ -86,7 +87,9 @@ def WriteMake(ab_dir,projectName,sSrcFile,sDLL,sLIB,sInclude,slibFolder,sInclude
         f.write('set(CMAKE_C_COMPILER   "C:/msys64/mingw64/bin/clang+")\n')
         f.write('set(CMAKE_EXE_LINKER_FLAGS "-static-libgcc -static-libstdc++")\n')
         f.write('include_directories(include)\n')
-        f.write('include_directories(E:/eigen-eigen-323c052e1731/eigen-eigen-323c052e1731)\n')
+        f.write('include_directories(E:/CPPTOOL/eigen337)\n')
+        f.write('include_directories(E:/CPPTOOL/nolhmann)\n')
+        f.write('include_directories(E:/CPPTOOL/MyTool/include)\n')
 
         f.write(
             'include_directories(D:/ProgramData/Anaconda3/include)\n')  # 还要把python的inlcude文件夹添加进来，因为有可能和python交互，用到python.h
@@ -146,23 +149,19 @@ def findEffectiveLocOfSign(sign, str_, startLoc):
     return (loc)
 
 
-
-
-
-
-
 def getRidOfTextBetweenDoubleQuote(str_):
     # find first有效位置
-    sign='\"'
-    loc = findEffectiveLocOfSign(sign,str_, 0)
+    sign = '\"'
+    loc = findEffectiveLocOfSign(sign, str_, 0)
     if loc == -1:
         result = str_
         return (str_)
     else:
-        loc2 = findEffectiveLocOfSign(sign,str_, loc + 1)
+        loc2 = findEffectiveLocOfSign(sign, str_, loc + 1)
         str_ = str_.replace(str_[loc:loc2 + 1], '')
         str_ = getRidOfTextBetweenDoubleQuote(str_)
         return (str_)
+
 
 def getRidOfTextBetweenSingleQuote(str_):
     # find first有效位置
@@ -178,34 +177,8 @@ def getRidOfTextBetweenSingleQuote(str_):
         return (str_)
 
 
-def findEffectiveLocOfLeftBrace(str_, startLoc):#查找有效左大括号位置
-    sign='{'
-    loc = str_.find(sign, startLoc)
-    if loc == -1:
-        return -1
-    locOfPreLinefeed = str_.rfind('\n', 0, loc)  # 最近的换行符位置
-    locOfPreAnnotation = str_.rfind('//', 0, loc)  # 最近的注释位置
-    if locOfPreLinefeed == -1:  # 此时是第一行，前面无换行符
-        if locOfPreAnnotation != -1:  # 前面有注释符
-            unEffectiveCase2 = True  # 属于无效位置
-        else:
-            unEffectiveCase2 = False
-    else:  # 前面有换行符
-        if locOfPreAnnotation != -1:  # 前面有注释符
-            if locOfPreAnnotation > locOfPreLinefeed:  # 属于无效位置
-                unEffectiveCase2 = True
-            else:
-                unEffectiveCase2 = False
-        else:
-            unEffectiveCase2 = False  # 属于有效位置
-    if unEffectiveCase2 == False:
-        return loc
-    else:  # 说明是无效位置，还要继续找
-        loc = findEffectiveLocOfSign(sign, str_, loc + 1)
-    return (loc)
-
-def findEffectiveLocOfRightBrace(str_, startLoc):#查找有效左大括号位置
-    sign='}'
+def findEffectiveLocOfLeftBrace(str_, startLoc):  # 查找有效左大括号位置
+    sign = '{'
     loc = str_.find(sign, startLoc)
     if loc == -1:
         return -1
@@ -231,26 +204,53 @@ def findEffectiveLocOfRightBrace(str_, startLoc):#查找有效左大括号位置
     return (loc)
 
 
-def getIndexOfCorreBrace(str_,locOfFirstLeftBrace,left_sign_counter,right_sign_counter):#查找对应右括号的位置
-    #left_sign_counter = 1
-    #right_sign_counter = 0
+def findEffectiveLocOfRightBrace(str_, startLoc):  # 查找有效左大括号位置
+    sign = '}'
+    loc = str_.find(sign, startLoc)
+    if loc == -1:
+        return -1
+    locOfPreLinefeed = str_.rfind('\n', 0, loc)  # 最近的换行符位置
+    locOfPreAnnotation = str_.rfind('//', 0, loc)  # 最近的注释位置
+    if locOfPreLinefeed == -1:  # 此时是第一行，前面无换行符
+        if locOfPreAnnotation != -1:  # 前面有注释符
+            unEffectiveCase2 = True  # 属于无效位置
+        else:
+            unEffectiveCase2 = False
+    else:  # 前面有换行符
+        if locOfPreAnnotation != -1:  # 前面有注释符
+            if locOfPreAnnotation > locOfPreLinefeed:  # 属于无效位置
+                unEffectiveCase2 = True
+            else:
+                unEffectiveCase2 = False
+        else:
+            unEffectiveCase2 = False  # 属于有效位置
+    if unEffectiveCase2 == False:
+        return loc
+    else:  # 说明是无效位置，还要继续找
+        loc = findEffectiveLocOfSign(sign, str_, loc + 1)
+    return (loc)
+
+
+def getIndexOfCorreBrace(str_, locOfFirstLeftBrace, left_sign_counter, right_sign_counter):  # 查找对应右括号的位置
+    # left_sign_counter = 1
+    # right_sign_counter = 0
     locLeft = findEffectiveLocOfLeftBrace(str_, locOfFirstLeftBrace + 1)
     locRight = findEffectiveLocOfRightBrace(str_, locOfFirstLeftBrace + 1)
-    if locRight==-1 and locLeft==-1:#全找不到
+    if locRight == -1 and locLeft == -1:  # 全找不到
         raise ('原cpp文件有错，括号不成对儿')
-    elif locRight==-1 and locLeft!=-1:#
+    elif locRight == -1 and locLeft != -1:  #
         raise ('原cpp文件有错，括号不成对儿')
-    elif locRight!=-1 and locLeft==-1:#找得到右，找不到左
+    elif locRight != -1 and locLeft == -1:  # 找得到右，找不到左
         right_sign_counter += 1
         if left_sign_counter == right_sign_counter:
             return (locRight)
         else:
-            result = getIndexOfCorreBrace(str_,locRight, left_sign_counter, right_sign_counter)
+            result = getIndexOfCorreBrace(str_, locRight, left_sign_counter, right_sign_counter)
             return (result)
-    else:#左右全找的到
+    else:  # 左右全找的到
         # right_sign_counter+=1
         # left_sign_counter+=1
-        if locRight<locLeft:
+        if locRight < locLeft:
             right_sign_counter += 1
             if left_sign_counter == right_sign_counter:
                 return (locRight)
@@ -258,48 +258,43 @@ def getIndexOfCorreBrace(str_,locOfFirstLeftBrace,left_sign_counter,right_sign_c
                 result = getIndexOfCorreBrace(str_, locRight, left_sign_counter, right_sign_counter)
                 return (result)
         else:
-            left_sign_counter+=1
-            if left_sign_counter == right_sign_counter:#应该是不可能
+            left_sign_counter += 1
+            if left_sign_counter == right_sign_counter:  # 应该是不可能
                 return (locRight)
             else:
                 result = getIndexOfCorreBrace(str_, locLeft, left_sign_counter, right_sign_counter)
                 return (result)
 
 
-
-
-
-
-def getContentWithoutBrace(str_):#去掉{}当中的内容
-    sData=str_.split('\n')
-    recursiveMark=False
-    lastline=''
-    lineLen=0
+def getContentWithoutBrace(str_):  # 去掉{}当中的内容
+    sData = str_.split('\n')
+    recursiveMark = False
+    lastline = ''
+    lineLen = 0
     lineCounter = 0
     for line in sData:
-        #1 同一行包括'namespace'或'class'且包括'{' 这样括号间的内容不可删除
-        #2 本行虽然没有'namespace'或'class'，且本行包括'{'，但上一行包括'namespace'或'class',且上一行不含'{' 这样括号间的内容不可删除
-        condition1=(('namespace' in line) or('class' in line))and ('{' in line)
-        condition2=((('namespace' not in line) and('class' not in line))and('{' in line)and
-                    (('namespace' in lastline)or('class' in lastline))and('{' not in lastline))
-        if condition1 or condition2:#不可删除，看下一行
+        # 1 同一行包括'namespace'或'class'且包括'{' 这样括号间的内容不可删除
+        # 2 本行虽然没有'namespace'或'class'，且本行包括'{'，但上一行包括'namespace'或'class',且上一行不含'{' 这样括号间的内容不可删除
+        condition1 = (('namespace' in line) or ('class' in line)) and ('{' in line)
+        condition2 = ((('namespace' not in line) and ('class' not in line)) and ('{' in line) and
+                      (('namespace' in lastline) or ('class' in lastline)) and ('{' not in lastline))
+        if condition1 or condition2:  # 不可删除，看下一行
             pass
         else:
             indexOfleftBrace = line.find('{')
-            if indexOfleftBrace!=-1:#找到了，那么就可以删除
+            if indexOfleftBrace != -1:  # 找到了，那么就可以删除
                 recursiveMark = True
-                indexOfStr=indexOfleftBrace+lineLen+lineCounter
-                endloc=getIndexOfCorreBrace(str_,indexOfStr,1,0)
-                str_=str_.replace(str_[indexOfStr:endloc+1],';')
+                indexOfStr = indexOfleftBrace + lineLen + lineCounter
+                endloc = getIndexOfCorreBrace(str_, indexOfStr, 1, 0)
+                str_ = str_.replace(str_[indexOfStr:endloc + 1], ';')
                 break
         lineCounter += 1
-        lineLen+=len(line)
-        lastline=line
+        lineLen += len(line)
+        lastline = line
     if not recursiveMark:
-        return(str_)
+        return (str_)
     else:
-        return(getContentWithoutBrace(str_))
-
+        return (getContentWithoutBrace(str_))
 
 
 def reWriteThisCPPFile(sourceFilePath):
@@ -307,44 +302,47 @@ def reWriteThisCPPFile(sourceFilePath):
         datas = f.readlines()
     sData = [line for line in datas if '#include' not in line]
     print(sourceFilePath.split('\\'))
-    headerName=sourceFilePath.split('\\')[-1].replace('.cpp','.h')
-    str_='#include"{}"\n'.format(headerName)
-    str_=str_+"".join(sData)
+    headerName = sourceFilePath.split('\\')[-1].replace('.cpp', '.h')
+    str_ = '#include"{}"\n'.format(headerName)
+    str_ = str_ + "".join(sData)
     with open(sourceFilePath, 'w', encoding='utf-8')as f:
         f.write(str_)
 
 
 def tell_if_is_templateFile(filepath):
-    with open(filepath,'r',encoding='utf-8')as f:sData=f.readlines()
-    templateMark=False
+    with open(filepath, 'r', encoding='utf-8')as f:
+        sData = f.readlines()
+    templateMark = False
     for line in sData:
         if line.strip():
             if ('template' in line):
-                if line.find('//')==-1:# 模板关键字并不在注释中
+                if line.find('//') == -1:  # 模板关键字并不在注释中
                     templateMark = True
                     break
-                elif line.find('template')<line.find('//'):# 模板关键字并不在注释中
+                elif line.find('template') < line.find('//'):  # 模板关键字并不在注释中
                     templateMark = True
                     break
     return (templateMark)
 
+
 def getHPPFileContent(filepath):
-    with open(filepath,'r',encoding='utf-8')as f:sData=f.readlines()
-    content=''.join(sData)
+    with open(filepath, 'r', encoding='utf-8')as f: sData = f.readlines()
+    content = ''.join(sData)
     return (content)
 
 
 def getHFileContent(filepath):
-    with open(filepath,'r',encoding='utf-8')as f:datas=f.readlines()
+    with open(filepath, 'r', encoding='utf-8')as f: datas = f.readlines()
     sData = [line for line in datas if '#include' not in line]
     sInclude_str = [line for line in datas if '#include' in line]
     str_ = "".join(sData)
-    str_ = getRidOfTextBetweenDoubleQuote(str_)#去掉正文中的小括号
+    str_ = getRidOfTextBetweenDoubleQuote(str_)  # 去掉正文中的小括号
     str_ = getRidOfTextBetweenSingleQuote(str_).strip()
-    str_=getContentWithoutBrace(str_)
-    str_include="".join(sInclude_str)+'\n'
-    str_=str_include+str_
+    str_ = getContentWithoutBrace(str_)
+    str_include = "".join(sInclude_str) + '\n'
+    str_ = str_include + str_
     return (str_)
+
 
 def autoReplenishFile():
     s = sys.argv
@@ -353,16 +351,16 @@ def autoReplenishFile():
     # 检查所有源文件是否有对应的头文件
     for file in set(sSrcFile):
         if 'main.cpp' not in file:
-            #1判断其对应的头文件是否存在
+            # 1判断其对应的头文件是否存在
             corresponding_header1 = file.replace('src', 'include').replace('.cpp', '.h')
             corresponding_header2 = file.replace('src', 'include').replace('.cpp', '.hpp')
-            if (os.path.isfile(corresponding_header1)==False)and(os.path.isfile(corresponding_header2)==False):
-                #此时可知其对应头文件不存在，应该自动补头文件
-                #2判断源文件中，是否包含template字段，且template字段不在注释中，
-                #如果包含那么就全部复制到HPP文件
+            if (os.path.isfile(corresponding_header1) == False) and (os.path.isfile(corresponding_header2) == False):
+                # 此时可知其对应头文件不存在，应该自动补头文件
+                # 2判断源文件中，是否包含template字段，且template字段不在注释中，
+                # 如果包含那么就全部复制到HPP文件
                 if tell_if_is_templateFile(file):
-                    print(file,'template')
-                    #若为真，那么全部复制进新的hpp文件
+                    print(file, 'template')
+                    # 若为真，那么全部复制进新的hpp文件
                     fileName = corresponding_header2.split('\\')[-1].replace('.hpp', '')
                     sfolder = corresponding_header2.split('\\')
                     s = []
@@ -389,12 +387,12 @@ def autoReplenishFile():
 
                         str_ = '\n#endif'
                         f.write(str_)
-                    #把源文件标志为弃用
-                    with open(file,'w')as f:
+                    # 把源文件标志为弃用
+                    with open(file, 'w')as f:
                         f.write('//OriginSrcFile contain template,deprecated')
                     sSrcFile.remove(file)
-                else:#这个时候不含tempalte，采用正常的分离模式
-                    print(file,'withouttemplate')
+                else:  # 这个时候不含tempalte，采用正常的分离模式
+                    print(file, 'withouttemplate')
 
                     fileName = corresponding_header1.split('\\')[-1].replace('.h', '')
                     sfolder = corresponding_header1.split('\\')
@@ -425,6 +423,7 @@ def autoReplenishFile():
                     reWriteThisCPPFile(file)  # 改头（把原来的include，变为include.h）
     return 0
 
+
 def pushShutcut():
     time.sleep(1)
     win32api.keybd_event(16, 0, 0, 0)  # shift
@@ -442,40 +441,49 @@ def examinFolder():
     ab_dir = s[1]
     findAllSrcFile(ab_dir + '/src/*')
     findAllSrcFile(ab_dir + '/MyTool/src/*')
+
     findALLLibFile(ab_dir + '/bin/*')
     findALLDllFile(ab_dir + '/bin/*')
-    findALLIncludeFile(ab_dir + '/include/*')
+
     findALLIncludeFile(ab_dir + '/MyTool/include/*')
-    findALLLibFile('E:/frequentlyUsedCPPLibrary/bin/*')
-    findALLDllFile('E:/frequentlyUsedCPPLibrary/bin/*')
-    findALLIncludeFile('E:/frequentlyUsedCPPLibrary/include/*')
+    findALLIncludeFile(ab_dir + '/include/*')
     print('项目源文件集合:', sSrcFile)
     print('项目动态库文件集合:', sDLL)
     print('项目静态库文件集合:', sLIB)
     print('项目头文件夹集合:', sInclude)
     print('项目库文件夹集合:', slibFolder)
     print('项目头文件夹集合:', sIncludefolder)
-    return(ab_dir,projectName,sSrcFile,sDLL,sLIB,sInclude,slibFolder,sIncludefolder)
-
+    return (ab_dir, projectName, sSrcFile, sDLL, sLIB, sInclude, slibFolder, sIncludefolder)
 
 
 def main():
     s = sys.argv
     projectDir = s[1]
-    try:os.makedirs(projectDir+'/bin/')
-    except:pass
-    try:os.makedirs(projectDir+'/src/')
-    except:pass
-    try:os.makedirs(projectDir+'/include/')
-    except:pass
-    try:os.makedirs(projectDir+'/MyTool/include/')
-    except:pass
-    try:os.makedirs(projectDir+'/MyTool/src/')
-    except:pass
+    try:
+        os.makedirs(projectDir + '/bin/')
+    except:
+        pass
+    try:
+        os.makedirs(projectDir + '/src/')
+    except:
+        pass
+    try:
+        os.makedirs(projectDir + '/include/')
+    except:
+        pass
+    try:
+        os.makedirs(projectDir + '/MyTool/include/')
+    except:
+        pass
+    try:
+        os.makedirs(projectDir + '/MyTool/src/')
+    except:
+        pass
     examinFolder()
     autoReplenishFile()
-    ab_dir,projectName,sSrcFile,sDLL,sLIB,sInclude,slibFolder,sIncludefolder=examinFolder()
-    WriteMake(ab_dir,projectName,sSrcFile,sDLL,sLIB,sInclude,slibFolder,sIncludefolder)
+    ab_dir, projectName, sSrcFile, sDLL, sLIB, sInclude, slibFolder, sIncludefolder = examinFolder()
+    WriteMake(ab_dir, projectName, sSrcFile, sDLL, sLIB, sInclude, slibFolder, sIncludefolder)
     return 0
+
 
 main()
